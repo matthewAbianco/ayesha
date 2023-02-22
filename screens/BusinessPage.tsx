8703e0c08fe45c45eeec1207805e4c1936c09b7e
unknown <matthewbianco@hotmail.com>
2023-02-22 17:15:40 -0500

    filled in some text spots, not much

--- screens/BusinessPage.tsx
+++ screens/BusinessPage.tsx
@@ -9,39 +9,77 @@ const BusinessPage = () => {
         <View>
           <View>{/* Cover Photo */}</View>
           <View>
+            <Text>Cover Photo</Text>
+          </View>
+          <View>
             {/* User/Business Name */}
-
+            <View>
+              <Text>Business name here</Text>
+            </View>
             {/* Joined Date */}
-
-            {/* Request To Book Button */}
-
+            <View>
+              <Text>Joined date</Text>
+            </View>
             {/* User profile Image */}
-
+            <View>
+              <Text>user profile image</Text>
+            </View>
             {/* Star Rating */}
-
-            {/* Junoir Or Senior at positon */}
+            <View>
+              <Text>users 1 - 5 star Rating</Text>
+            </View>
+            {/* Novice, Apprentice, Journeyman, Master*/}
+            <View>
+              <Text>users 1 - 5 star Rating</Text>
+            </View>
           </View>
         </View>
         {/* Social Media and Jobs Completed */}
         <View>
+          <Text>social media and jobs completed</Text>
+        </View>
+        <View>
           <View>{/* Social Media Links */}</View>
+          <View>
+            <Text>Twitter</Text>
+            <Text>Instagram</Text>
+            <Text>TikTok</Text>
+            <Text>Facebook</Text>
+            <Text>Business Website</Text>
+          </View>
           <View>{/* Jobs Completed */}</View>
+          <View>
+            <Text>Jobs Completed: 3</Text>
+          </View>
         </View>
         <View>
           <View>{/* Bio of business or person */}</View>
-          {/* <View> specialities</View>  */}
-          {/* <view>Customer review icons</view> */}
+          <View>
+            <Text>Specialities</Text>
+          </View>
+          <View>Customer review icons</View>
+          <View>
+            <View>Icon</View>
+            <View>Icon</View>
+            <View>Icon</View>
+            <View>Icon</View>
+            <View>Icon</View>
+            <View>Icon</View>
+          </View>
         </View>
-        {/* Packages and services offered */}
+        {/* Services offered */}
         <View>
-          {/* <Text> Services Offered</Text> */}
-          <View>{/* Services offered */}</View>
+          <Text> Services Offered</Text>
+          <View></View>
           <View>{/* <View> Logos of companies worked for </View> */}</View>
           {/* User and Business Review */}
           <View>
             <View>{/* User and business review */}</View>
+            <View>
+              <Text>Business Reviews</Text>
+              <View>Review Cards Here!</View>
+            </View>
           </View>
-          <View>{/* Favourite Location */}</View>
           <View></View>
         </View>
       </SafeAreaView>
