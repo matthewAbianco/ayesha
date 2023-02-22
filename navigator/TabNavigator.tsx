abfac086f666be73b73b62dba8d99c6bfecacbb7
unknown <matthewbianco@hotmail.com>
2023-02-22 12:08:22 -0500

    fixed navigation headers

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,29 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import BusinessPage from '../screens/BusinessPage';
-import JobBoard from '../screens/JobBoard';
+import { View, Text } from "react-native"
+import React from "react"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import BusinessPage from "../screens/BusinessPage"
+import JobBoard from "../screens/JobBoard"
+import { useLayoutEffect } from "react"
+import { useNavigation } from "@react-navigation/native"
 
 export type TabParamList = {
-    BusinessPage: undefined;
-    JobBoard: undefined;
+  BusinessPage: undefined
+  JobBoard: undefined
 }
 
-const Tab = createBottomTabNavigator<TabParamList>();
+const Tab = createBottomTabNavigator<TabParamList>()
 
 const TabNavigator = () => {
+  const navigation = useNavigation()
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
 
-
-
-    return (
-        <Tab.Navigator screenOptions={({ route }) => ({
-            tabBarActiveTintColor: '#59C1CC',
-            tabBarInactiveTintColor: 'gray',
-        })} >
-            <Tab.Screen name="BusinessPage" component={BusinessPage} />
-            <Tab.Screen name="JobBoard" component={JobBoard} />
-        </Tab.Navigator>
-    )
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen
+        name="BusinessPage"
+        options={{ headerShown: false }}
+        component={BusinessPage}
+      />
+      <Tab.Screen
+        name="JobBoard"
+        options={{ headerShown: false }}
+        component={JobBoard}
+      />
+    </Tab.Navigator>
+  )
 }
 
 export default TabNavigator
